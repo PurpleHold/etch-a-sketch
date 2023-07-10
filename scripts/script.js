536f80f046a181c3f1efd7374db527b76af8d7e6
PurpleHold <130397464+PurpleHold@users.noreply.github.com>
2023-07-10 18:12:50 +0200

    Keep same sketch area with different user inputs

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,12 @@ const askPixels = document.querySelector('#pixels');
 console.log(askPixels);
 
 let manyDivs = "";
-for (i=0; i<9999; i++) {
-    manyDivs += "<div></div>";
+for (i=0; i<1024; i++) {
+    manyDivs += '<div class="new-div"></div>';
 }
 gridContainer.innerHTML = manyDivs;
-/*
-for (i=0; i<256; i++) {
-    gridContainer.innerHTML += "<div></div>";
-}
-*/
-const allSquares = document.querySelectorAll('.grid-container > div');
+
+const allSquares = document.querySelectorAll('.new-div');
 console.log(allSquares);
 
 allSquares.forEach(square => {
@@ -25,16 +21,26 @@ allSquares.forEach(square => {
 let newPixelsNb;
 
 askPixels.onclick = () => {
-    newPixelsNb = Number(prompt("Choose the number of squares per side (default 16, max is 100)"));
+    newPixelsNb = Number(prompt("Choose the number of squares per side (default 32, max is 100)"));
     manyDivs = "";
     for (i=0; i<(newPixelsNb*newPixelsNb); i++) {
-        manyDivs += "<div></div>";
+        manyDivs += '<div class="new-div"></div>';
     }
     gridContainer.innerHTML = manyDivs;
-    const allSquares = document.querySelectorAll('.grid-container > div');
+    const allSquares = document.querySelectorAll('.new-div');
+
+    let newWidth = ((Math.round((85/newPixelsNb)*100))/100) + "%";
+    const newDivs = document.getElementsByClassName('new-div');
+    for(i = 0; i < newDivs.length; i++) {
+    newDivs[i].style.width = newWidth;
+    }
+
     allSquares.forEach(square => {
         square.addEventListener("mouseover", (square) => {
             square.target.style.backgroundColor = "red";
         })
     });
 };
+
+/*let newWidth = ((Math.round((85/newPixelsNb)*100))/100) + "%";
+let newWidth = Math.round((80/newPixelsNb)*10) + "px";*/
